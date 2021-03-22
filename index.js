0ad9ee654daee717841a1bb5d915cf377562d5c5
Pascal Joly <jolypascal20@gmail.com>
2021-03-22 16:33:42 +0100

    Add files

---
+++ index.js
@@ -0,0 +1,8 @@
+const chalk = require ('chalk');
+
+
+console.log(chalk.red('Mathis'));
+console.log(chalk.yellow('Lou-Ann'));
+console.log(chalk.green('Enzo'));
+console.log(chalk.blue('Cathy'));
+
